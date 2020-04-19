239f2367c3f664ac4e8b3f006541e1037ab55d3c
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-19 14:16:16 +0700

    Cleanup project

--- MadeForScan/ViewController.swift
+++ MadeForScan/ViewController.swift
@@ -1,20 +1,6 @@
-//
-//  ViewController.swift
-//  MadeForScan
-//
-//  Created by Wasith Theerapattrathamrong on 19/4/2563 BE.
-//  Copyright © 2563 Wasith Theerapattrathamrong. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
 
 }
 
